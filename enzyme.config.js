a11bd1e81b9a9ebcdc773363c5731f0d5b88469a
Breno Sena <breno.fsena@gmail.com>
2020-08-05 01:33:03 -0300

    fix: enzyme configuration

--- enzyme.config.js
+++ enzyme.config.js
@@ -2,5 +2,15 @@ import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({
-  adapter: new Adapter()
+  adapter: new Adapter(),
 });
+
+window.matchMedia =
+  window.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    };
+  };
