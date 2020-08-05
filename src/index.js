0afbc2c80f2715f3339e168b7a8e14aa96d50015
Breno Sena <breno.fsena@gmail.com>
2020-08-05 01:35:41 -0300

    chore: change styles folder nomenclature

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import "./sass/style.scss";
+import "./style/style.scss";
 import * as offline from "offline-plugin/runtime";
 
 offline.install({
