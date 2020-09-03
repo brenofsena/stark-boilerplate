5b5eb02ce69bce1e124c2c072c4739b03401f4f2
Breno Sena <breno.fsena@gmail.com>
2020-09-03 18:28:58 -0300

    refactor: simplify code

--- src/components/spinner/spinner.spec.js
+++ src/components/spinner/spinner.spec.js
@@ -3,13 +3,9 @@ import { screen } from '@testing-library/react';
 import { renderWithTheme } from '../../utils/test/helpers';
 import Spinner from './spinner';
 
-const makeSut = () => {
-  renderWithTheme(<Spinner />);
-};
-
 describe('Spinner Component', () => {
   test('Should render with 4 divs', () => {
-    makeSut();
+    renderWithTheme(<Spinner />);
     expect(screen.getByTestId('spinner').childElementCount).toBe(4);
   });
 });
