9627e0c8fc7d10fef8123642f7c2bab8498e9563
Breno Sena <breno.fsena@gmail.com>
2020-09-28 20:29:17 -0300

    fix: @ and @assets alias on jest config

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,8 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/src/test/cypress'],
   testEnvironment: 'jsdom',
   moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '@assets/(.*)': '<rootDir>/static/$1',
     '\\.css$': 'identity-obj-proxy',
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
